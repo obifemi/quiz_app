241b7b716d8fb3b99ff9135ab20c6551f0936c3a
obifemi <54670899+obifemi@users.noreply.github.com>
2023-07-26 19:35:30 +0000

    add router

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
