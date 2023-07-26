84f6beb6d1acccde290c277eec91d24444392055
obifemi <54670899+obifemi@users.noreply.github.com>
2023-07-26 20:00:58 +0000

    add routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,26 @@
 import '../styles/App.css';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
+const router = createBrowserRouter([
+{
+path:'/',
+element: <div>Root</div>
+},
+{
+path:'/quiz',
+element: <div>Quiz</div>
+},
+{
+path:'/result',
+element: <div>Result</div>
+}
+])
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+  <>
+  <RouterProvider router={router}/>
+  </>
   );
 }
 
