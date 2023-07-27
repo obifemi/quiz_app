be0137d35c8ba309eab021b203633262ee420d8a
obifemi <54670899+obifemi@users.noreply.github.com>
2023-07-27 06:51:15 +0000

    add quiz funcs

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
 export default function Quiz() {
+
+  function onNext(){
+    console.log('next')
+  }
+  function onPrev(){
+    console.log('prev')
+  }
+
   return (
-    <div>Quiz</div>
+    <div className='container'>
+          <h1 className='title text-light'>Quiz App</h1>
+
+          <div className="grid">
+            <button className='btn prev' onClick={onPrev}>Prev</button>
+            <button className='btn next' onClick={onNext}>Next</button>
+          </div>
+    </div>
   )
 }
