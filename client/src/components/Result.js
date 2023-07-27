7bd448fc1ecf2c64a9f7ff018b877d68ec3560e6
obifemi <54670899+obifemi@users.noreply.github.com>
2023-07-27 16:02:55 +0000

    add result

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import '../styles/Result.css'
+import {Link} from 'react-router-dom'
 
 export default function Result() {
+  const onRestart = function(){
+    console.log('onRestart')
+  }
   return (
-    <div>Result</div>
+    <div className="container">
+      <h1 className="title text-light">
+        Total Quiz Point
+      </h1>
+      <div className="result flex-center">
+        <div className="flex">
+      <span>Username:</span>
+      <span className="bold">Mert</span>
+      </div>
+        <div className="flex">
+      <span>Total Questions:</span>
+      <span className="bold">50</span>
+      </div>
+        <div className="flex">
+      <span>Total Attempts:</span>
+      <span className="bold">5</span>
+      </div>
+        <div className="flex">
+      <span>Total Earn Points:</span>
+      <span className="bold">5</span>
+      </div>
+        <div className="flex">
+      <span>Quiz Result:</span>
+      <span className="bold">5</span>
+      </div>
+      </div>
+      <div className="start">
+      <Link className='btn' to={'/'} onClick={onRestart}>Restart</Link>
+      </div>
+    </div>
   )
 }
